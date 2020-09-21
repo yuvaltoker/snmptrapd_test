a00ddbd7ec558f43f443953cf147b464c9d1549b
YuvalToker <yuval@gmail.com>
2020-09-21 14:40:55 +0300

    final changes were made & added READ_ME file.

--- dock_test/instructions.sh
+++ dock_test/instructions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+declare -i x=1
 while true; do
     sleep 5
-    snmptrap -v2c -c public testing_snmptrapd:162 '' SNMPv2-MIB::coldStart.0 SNMPv2-MIB::sysContact.0 s 'yep22'
+    snmptrap -v2c -c public testing_snmptrapd:162 '' SNMPv2-MIB::coldStart.0 SNMPv2-MIB::sysContact.0 s 'trap number #'$x
+    x=x+1
 done
 
