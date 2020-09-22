172a6d78d6c68ccf8e1b7a726957c7fcdd249c0f
YuvalToker <yuval@gmail.com>
2020-09-22 10:11:24 +0300

    final changes and code cleaning

--- dock_snmptrapd/instructions.sh
+++ dock_snmptrapd/instructions.sh
@@ -2,16 +2,13 @@
 
 
 
-# In case of ? - see if it's working without it, in that case you 
-# can delete it
-
-echo "snmpd:127.0.0.1" >> /etc/hosts.allow # ?
+echo "snmpd:127.0.0.1" >> /etc/hosts.allow
 echo "snmptrapd: 127.0.0.1" >> /etc/hosts.allow
-echo "master agentx" >> /etc/snmp/snmpd.conf # ?
+echo "master agentx" >> /etc/snmp/snmpd.conf # for the futue snmpd, doesn't belong to the current example
 echo "authCommunity   log,execute,net public" >> /etc/snmp/snmptrapd.conf
 echo "forward default unix:/var/opt/FJSVssmgr/trap_socket" >> /etc/snmp/snmptrapd.conf
 echo "traphandle SNMPv2-MIB::coldStart    /tmp/go cold" >> /etc/snmp/snmptrapd.conf
 echo "traphandle SNMPv2-MIB::sysContact   /tmp/go contact" >> /etc/snmp/snmptrapd.conf
 cp ./go /tmp/
-snmptrapd -f -Lf file.log -c /etc/snmp/snmptrapd.conf -M /usr/share/snmp/mibs --master=agentx --agentXSocket=tcp:localhost:161 udp:162
+snmptrapd -f -Lf file.log -c /etc/snmp/snmptrapd.conf -M /usr/share/snmp/mibs --agentXSocket=tcp:localhost:161 udp:162
 
